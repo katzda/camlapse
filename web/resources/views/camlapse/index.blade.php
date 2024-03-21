2c79c32d8285e11f8dbd251fb8dca24377c9e4d9
Dan Katz <danekkatz@gmail.com>
2024-03-21 22:12:56 +0000

    arrow

--- web/resources/views/camlapse/index.blade.php
+++ web/resources/views/camlapse/index.blade.php
@@ -14,20 +14,21 @@
 
     <div class="grid gap-6 lg:grid-cols-2 lg:gap-8">
         @foreach ($all as $camlapse)
-            <x-tile>
-                {{-- fph
-                between_hour_start
-                between_hour_end
-                memory_period
-                stop_datetime --}}
+            <a href="{{ route('camlapse.show', ['camlapse' => $camlapse['id']]) }}">
+                <x-tile>
+                    {{-- fph
+                    between_hour_start
+                    between_hour_end
+                    memory_period
+                    stop_datetime --}}
 
-                <div>
-                    <a href="{{ route('camlapse.show', ['camlapse' => $camlapse['id']]) }}" class="text-4xl">
-                        <h2>{{$camlapse['name']}}<h2>
-                    </a>
-                    <span>{{ $camlapse['fph'] }} photoes / hour</span>
-                </div>
-            </x-tile>
+                    <div>
+                        <h2 class="text-4xl">{{$camlapse['name']}}<h2>
+                        <span>{{ $camlapse['fph'] }} photoes / hour</span>
+                    </div>
+                    <x-arrow-r />
+                </x-tile>
+            </a>
         @endforeach
     </div>
 @endsection
