0662b8d463d30bcb984123304313c3cf6a7f0da0
Dan Katz <danekkatz@gmail.com>
2024-06-28 12:55:55 +0100

    d

--- web/app/Console/Commands/TakeSnapshot.php
+++ web/app/Console/Commands/TakeSnapshot.php
@@ -57,6 +57,7 @@ class TakeSnapshot extends Command
 
             if($this->saveCameraSnapshot($camlapse, $now)){
                 $this->info($camlapse->name.": snap!");
+                $this->updateVideo($camlapse);
             }
         }
     }
@@ -77,7 +78,7 @@ class TakeSnapshot extends Command
 
         // Define the temporary file path
         $file = '/' . str_replace(" ", "T", $now->toDateTimeString()) . '.jpg';
-        $dir = 'images/'.$camlapse->id;
+        $dir = 'images/'.$camlapse->id."/photos";
 
         if (!Storage::exists($dir)) {
             Storage::makeDirectory($dir);
@@ -89,41 +90,29 @@ class TakeSnapshot extends Command
         return true;
     }
 
-    private function updateVideo(CamLapse $camlapse, Carbon $now) {
-        # Directory containing the images
-        $directory = Storage::path("images/".$camlapse->id."/");
+    private function updateVideo(CamLapse $camlapse) {
+        $baseDir = Storage::path("images/".$camlapse->id."/");
+        $photos = glob($baseDir . "photos/*.jpg", GLOB_BRACE);
+        $output_dir = $baseDir . "video/";
 
-        # Get all jpg files and sort them
-        $files = glob($directory . "*.jpg", GLOB_BRACE);
-        sort($files);
+        if (!Storage::exists($output_dir)) {
+            Storage::makeDirectory($output_dir);
+        }
 
-        # Count the total number of images
-        $total_images = count($files);
-
-        # Desired video length in seconds
+        $total_images = count($photos);
         $video_length = 3;
-
-        # Calculate frame rate
         $frame_rate = round($total_images / $video_length, 2);
 
         # Generate the input file list for ffmpeg
-        $file_list = $directory . "filelist.txt";
+        sort($photos);
+        $file_list = $baseDir . "filelist.txt";
         $handle = fopen($file_list, "w");
-
-        foreach ($files as $file) {
+        foreach ($photos as $file) {
             fwrite($handle, "file '$file'\n");
         }
-
         fclose($handle);
 
-        # Path to the output video file
-        $output_file = $directory . "video.mp4";
-
-        # Execute the ffmpeg command
-        $command = "ffmpeg -f concat -safe 0 -r $frame_rate -i $file_list -c:v libx264 -pix_fmt yuv420p $output_file";
-        shell_exec($command);
-
-        # Optionally, clean up the file list
+        shell_exec("ffmpeg -f concat -safe 0 -r $frame_rate -i $file_list -c:v libx264 -pix_fmt yuv420p $output_dir/video.mp4");
         unlink($file_list);
     }
 }
