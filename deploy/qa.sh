ad0898c555d91daa17e4faa4ccaaa83110826d0c
Dan Katz <danekkatz@gmail.com>
2024-06-28 22:04:49 +0100

    scripts

---
+++ deploy/qa.sh
@@ -0,0 +1,12 @@
+# run build script with prod settings
+
+# save image
+docker save --output saved-image.tar my-image:1.0.0
+
+# transfer image
+scp
+
+# ssh
+copy secrets
+docker load --input saved-image.tar
+docker run mounting secrets
