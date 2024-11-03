b9746546e1147967cdef5975f8d8ab0e12d30216
Dan Katz <danekkatz@gmail.com>
2024-11-03 19:33:08 +0000

    switched to php:8.3-fpm-alpine

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,40 +1,36 @@
-#!/bin/bash
+#!/bin/sh
 
-chgrp developer /dev/video*; \
+chgrp www-data /dev/video*; \
 chmod 775 /dev/video*;
 
 # dockerfile, esp for prod, might have created all laravel files with default uid gid, so I need to check and update them
 CURR_OWN=$(ls -ld ./storage | awk '{print $3}');
-if [ "$CURR_OWN" != "developer" ];
+if [ "$CURR_OWN" != "www-data" ];
 then
   echo "changing ownership of all files, pls be patient, this can take a long time";
-  chown -R developer .;
+  chown -R www-data .;
 fi;
 
 CURR_GRP=$(ls -ld ./storage | awk '{print $4}');
-if [ "$CURR_GRP" != "developer" ];
+if [ "$CURR_GRP" != "www-data" ];
 then
   echo "changing group on all files, pls be patient, this can take a long time";
-  chgrp -R developer .;
+  chgrp -R www-data .;
 fi;
 
 #ONLY NOW I CAN START WRITING INTO THE LARAVEL DIRECTORY
 #create empty directories, because in production laravel might complain that paths don't exist even if redis is used
 
-gosu developer mkdir -p \
-  /home/developer/apache2 \
+gosu www-data mkdir -p \
   storage/framework/cache/data \
   storage/framework/sessions \
   storage/framework/testing \
   storage/framework/views;
 
-set -a
-gosu developer cp /run/secrets/env &BASE_DIR&/.env;
-
 # some of these can't be in dockerfile probably because of .env values being cached too early
-gosu developer php artisan config:clear;
-gosu developer php artisan view:clear;
-gosu developer php artisan route:clear;
-gosu developer php artisan event:clear;
+gosu www-data php artisan config:clear;
+gosu www-data php artisan view:clear;
+gosu www-data php artisan route:clear;
+gosu www-data php artisan event:clear;
 
-/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf;
+/usr/bin/supervisord -n -c /etc/supervisord.conf;
